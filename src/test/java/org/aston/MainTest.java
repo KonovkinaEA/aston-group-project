5368f9550c86b65f334b744005cc70e584f83006
liz(s)a <el.konovkina@mail.ru>
2024-08-08 19:04:33 +0300

    Initial commit

---
+++ src/test/java/org/aston/MainTest.java
@@ -0,0 +1,11 @@
+package org.aston;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void testExample() {
+        Assertions.assertTrue(true);
+    }
+}
