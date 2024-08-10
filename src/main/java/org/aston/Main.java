f15f0d6c555b93063c1fb222add3ca13cbdaaa84
liz(s)a <el.konovkina@mail.ru>
2024-08-10 20:10:48 +0300

    Basic model classes have been created

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -1,7 +1,31 @@
 package org.aston;
 
+import org.aston.model.Bus;
+import org.aston.model.User;
+import org.aston.model.Student;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bus bus = new Bus.Builder()
+                .setNumber("123-ABC")
+                .setModel("Volvo")
+                .setMileage(50000)
+                .build();
+
+        User user = new User.Builder()
+                .setName("liz(s)a")
+                .setPassword("password123")
+                .setEmail("lizsa@mail.com")
+                .build();
+
+        Student student = new Student.Builder()
+                .setGroupNumber("CS-101")
+                .setAverageGrade(4.5)
+                .setRecordBookNumber("RB123456")
+                .build();
+
+        System.out.println(bus);
+        System.out.println(user);
+        System.out.println(student);
     }
 }
---
+++ src/main/java/org/aston/model/User.java
@@ -0,0 +1,55 @@
+package org.aston.model;
+
+public class User {
+    private final String name;
+    private final String password;
+    private final String email;
+
+    private User(Builder builder) {
+        this.name = builder.name;
+        this.password = builder.password;
+        this.email = builder.email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public static class Builder {
+        private String name;
+        private String password;
+        private String email;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User(name=" + name + ", password=" + password + ", email=" + email + ')';
+    }
+}
