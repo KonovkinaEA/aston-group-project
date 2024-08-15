34cc9dd88a23bf20bfc5d043a4684ea779b871b7
Ivan <147175973+ivan211134@users.noreply.github.com>
2024-08-15 15:42:18 +0300

    Merge pull request #1 from KonovkinaEA/base-classes-creation
    Basic model classes have been created

---
+++ src/main/java/org/aston/RederFile/Parse/BusParser.java
@@ -0,0 +1,19 @@
+package org.aston.RederFile.Parse;
+import org.aston.model.Bus;
+
+public class BusParser implements RecordParser<Bus> {
+    @Override
+    public String getPrefix() {
+        return "Bus";
+    }
+
+    @Override
+    public Bus parse(String line) {
+        String[] parts = line.split(",");
+        return new Bus.Builder()
+                .setNumber(parts[0].trim())
+                .setModel(parts[1].trim())
+                .setMileage(Integer.parseInt(parts[2].trim()))
+                .build();
+    }
+}
--- src/main/java/org/aston/RederFile/ReaderFiles.java
+++ src/main/java/org/aston/RederFile/ReaderFiles.java
@@ -5,91 +5,36 @@ import java.io.*;
 import java.util.*;
 
 import org.aston.model.*;
+import org.aston.RederFile.Parse.*;
 
 public class ReaderFiles<T>  {
 
 
-
-
-    ///choice - Enter which class we are looking for(Bus/User/Student)
-    public List<T> ReadFiles(T f) {
-        String filePath = ".\\Resurs\\file.txt";
+    public List<T> ReadFiles(T f,String filePath ) {
         List<T> list = new ArrayList<T>();
-        String FieldClass1 = "";
-        String FieldClass2 = "";
-        String FieldClass3 ="";
         try (FileReader fileReader = new FileReader(filePath);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String line;
-
-
             while ((line = bufferedReader.readLine()) != null)
             {
-
-                FieldClass1 = "";
-                FieldClass2 = "";
-                FieldClass3 ="";
-
-                int switchClass = 0;
-
-                int  switchFieldClass = 0;
-
                 if((line.indexOf("Bus")>=0)&&(f.getClass()==Bus.class))
                 {
-                    switchClass=1;
-
-                    line =	line.replace("Bus = ","");
+                    BusParser bus = new BusParser();
+                    list.add((T) bus.parse(line));
                 }
 
-                if((line.indexOf("Student")>=0)&&(f.getClass() == Student.class))
+                if((line.indexOf("Student")>=0)&&(f.getClass()==Student.class))
                 {
-                    switchClass=2;
-
-                    line =	line.replace("Student = ","");
+                    StudentParser student = new StudentParser();
+                    list.add((T) student.parse(line));
                 }
 
-                if((line.indexOf("User")>=0)&&(f.getClass() == User.class))
+                if((line.indexOf("User")>=0)&&(f.getClass()==User.class))
                 {
-                    switchClass=3;
-                    line =	line.replace("User = ","");
-                }
-
-                char[] charArray = line.toCharArray();
-                for (char chr : charArray) {
-                    if ((chr != ',' )&&(chr != '\n')) {
-                        if(switchFieldClass == 0)
-                            FieldClass1 += chr;
-                        if(switchFieldClass == 1) {
-                            FieldClass2 += chr;
-                        }
-                        if(switchFieldClass == 2) {
-                            FieldClass3 +=chr;
-
-                        }
-                    }
-                    else
-                    {
-                        switchFieldClass++;
-                    }
-
-                }
-                if(switchClass==1)
-                {
-                    Bus bus = new Bus.Builder().setNumber(FieldClass1).setModel(FieldClass2).setMileage(Integer.parseInt(FieldClass3.trim())).build();
-                    list.add((T) bus);
-                }
-                if(switchClass==2)
-                {
-                    Student student = new Student.Builder().setGroupNumber(FieldClass1).setAverageGrade(Double.parseDouble(FieldClass2)).setRecordBookNumber(FieldClass3).build();
-                    list.add((T) student);
-                }
-                if(switchClass==3)
-                {
-                    User user = new User.Builder().setName(FieldClass1).setPassword(FieldClass2).setEmail(FieldClass3).build();
-                    list.add((T) user);
+                    UserParser user = new UserParser();
+                    list.add((T) user.parse(line));
                 }
             }
-
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
