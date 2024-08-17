2fce05de434996ff0b67f050cefb31ac60e28b09
wyzzy892 <ilgizn395@gmail.com>
2024-08-17 18:50:54 +0300

    Modify even sort file

--- src/main/java/org/aston/sorting/SelectionSortEven.java
+++ src/main/java/org/aston/sorting/SelectionSortEven.java
@@ -10,18 +10,18 @@ public class SelectionSortEven<T> implements Sorting<T> {
     public List<T> sort(List<T> arr, Comparator<T> comparator){
         List<T> sorted = new ArrayList<T>(arr);
         int n = sorted.size();
-        for(int i = 0; i < n - 1; i++){
-            int minIdx = i;
-            T minValue = sorted.get(minIdx);
-            if
-            for(int j = i + 1; j < n; j++) {
-                if (comparator.compare(sorted.get(j), minValue) < 0) {
-                    minIdx = j;
-                    minValue = sorted.get(minIdx);
-                }
-            }
-            Collections.swap(sorted, i, minIdx);
-        }
+        //TODO implement even sort
+//        for(int i = 0; i < n - 1; i++){
+//            int minIdx = i;
+//            T minValue = sorted.get(minIdx);
+//            for(int j = i + 1; j < n; j++) {
+//                if (comparator.compare(sorted.get(j), minValue) < 0) {
+//                    minIdx = j;
+//                    minValue = sorted.get(minIdx);
+//                }
+//            }
+//            Collections.swap(sorted, i, minIdx);
+//        }
         return sorted;
     }
 }
