afaece6185e2d4bcdffef833713fb647a4b7eaf2
Your David <davidgarciagamboa971015@gmail.com>
2024-08-17 17:25:02 -0500

    Adding to main

--- src/main/java/org/aston/Main.java
+++ src/main/java/org/aston/Main.java
@@ -1,19 +1,35 @@
 package org.aston;
 
+import org.aston.manual_creation.ManualBuilder;
+import org.aston.model.Bus;
+import org.aston.model.Student;
+import org.aston.model.User;
 import org.aston.random_creation.RandomCreator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ManualBuilder manualBuilder = new ManualBuilder(scanner);
+        List<Bus> buses = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
+        List<User> users = new ArrayList<>();
 
         boolean run = true;
         while (run) {
-            System.out.println("Чтобы выбрать следующий шаг, введите соответствующую цифру:");
-            System.out.println("0 - Завершить программу");
-            System.out.println("1 - Ввести исходные данные");
-            System.out.println("3 - Создание случайного массива заданной величины");
+            System.out.println("""
+                        *************************** МЕНЮ ***************************
+                        Чтобы выбрать следующий шаг, введите соответствующую цифру:
+                        0 - Завершить программу
+                        1 - Ввести исходные данные
+                        2 - Заполните данные вручную
+                        3 - Создание случайного массива заданной величины
+                        *************************************************************
+                    """);
+
             // TODO: добавить выборы на сортировку, бин. поиск
 
             switch (scanner.nextInt()) {
@@ -26,7 +42,32 @@ public class Main {
                     // TODO: добавить создание массива и заполнения массива
                     break;
                 case 2:
+                    System.out.println("Введите тип объекта, который вы хотите создать: Автобус, Студент, Пользователь.");
+                    String type = scanner.nextLine();
 
+                    switch (type) {
+                        case "Автобус":
+                            Bus bus = manualBuilder.createBus(scanner);
+                            if (bus != null) {
+                                buses.add(bus);
+                            }
+                            break;
+                        case "Студент":
+                            Student student = manualBuilder.createStudent(scanner);
+                            if (student != null) {
+                                students.add(student);
+                            }
+                            break;
+                        case "Пользователь":
+                            User user = manualBuilder.createUser(scanner);
+                            if (user != null) {
+                                users.add(user);
+                            }
+                            break;
+                        default:
+                            System.err.println("Неверный тип.");
+                            break;
+                    }
 
 
                 case 3:
